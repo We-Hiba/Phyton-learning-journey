1014ec952e6814ee6846c07285492d6d5fcc448b
We-Hiba <hibaporo3@gmail.com>
2025-09-05 18:31:54 +0900

    Update sum_two_integers.py

--- sum_two_integers.py
+++ sum_two_integers.py
@@ -2,13 +2,14 @@ a = 2
 b = 3
 c = a + b
 
-print("The sum of a and b is : " , c )    -> The sum of a and b is : 5
+print("The sum of a and b is : " , c )   #the result after running the code on python -> The sum of a and b is : 5
 
-print(f"The sum of {a} and {b} is {c} " )   -> The sum of 2 and 3 is : 5 
-
-
+print(f"The sum of {a} and {b} is {c} " )   #The result after running the code on python -> The sum of 2 and 3 is : 5 
 
 
-In the first exemple we see that the sentence inside the quotes will result in the exact sentence written inside exept of c that result in the value of it .
-In the second exemple the result of a and b is different because adding f before oppening the quotes will allow you to get the values of a and b if you put them inside these { } , same thing for c , you can include it inside the quotes and not after it .
+
+
+#In the first exemple we see that the sentence inside the quotes will result in the exact sentence written inside exept of c that result in the value of it .
+#In the second exemple the result of a and b is different because adding f before oppening the quotes will allow you to get the values of a and b if you put them inside these { } , same thing for c , you can include it inside the quotes and not after it .
+
 
